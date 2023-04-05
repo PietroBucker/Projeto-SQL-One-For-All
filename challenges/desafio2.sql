d474a85eb47bfad6641e4a80e7a9f08b4866bcfb
Pietro Bucker <pietro_177@hotmail.com>
2023-04-05 15:32:06 -0300

    requisito 2

---
+++ challenges/desafio2.sql
@@ -0,0 +1,14 @@
+SELECT 
+	  COUNT(mu.music_id) AS cancoes,
+    COUNT(DISTINCT(art.artist_id)) AS artistas,
+	  COUNT(DISTINCT(alb.album_id)) AS albums
+  FROM 
+	  SpotifyClone.music AS mu
+INNER JOIN 
+	  SpotifyClone.album AS alb
+  ON 
+	  mu.album_id = alb.album_id
+INNER JOIN 
+	SpotifyClone.artist AS art
+  ON 
+	  alb.artist_id = art.artist_id;
