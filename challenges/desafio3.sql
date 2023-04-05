d746a2ddfc1dcd0bb3ee6febc02cb690b5371715
Pietro Bucker <pietro_177@hotmail.com>
2023-04-05 16:53:40 -0300

    requisito 3 query para mostra quantas musica e tempo de rep de cada usuario

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,1 +1,11 @@
-desda
+SELECT 
+	u.user_name AS pessoa_usuaria,
+	COUNT(h.music_id)  AS musicas_ouvidas,
+    ROUND(SUM(m.music_duration)/60, 2) AS total_minutos
+FROM SpotifyClone.`history` AS h
+INNER JOIN SpotifyClone.`user` AS u
+ON h.user_id = u.user_id
+INNER JOIN SpotifyClone.music AS m
+ON h.music_id = m.music_id
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria;
